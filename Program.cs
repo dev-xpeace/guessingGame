23084278b08cd57265a2e30b847298887dc87877
dev-xpeace <xpeacedev@outlook.com>
2023-03-17 16:10:18 +0300

    guessingGame
    Basic Guessing Game

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+string secretWord = "devxpeace";
+string guess = "";
+int guessCount  = 0;
+int guessLimit = 3;
+bool outOfGuesses = false;
+
+while (guess != secretWord && !outOfGuesses)
+{
+    if (guessCount < guessLimit)
+    {
+        Console.Write("Enter Guess: ");
+        guess = Console.ReadLine();
+        guessCount++;
+    }
+    else
+    {
+        outOfGuesses = true;
+    }
+    {
+        if (outOfGuesses)
+        {
+            Console.WriteLine("You Lose!");
+        }
+        if (guess == secretWord)
+        {
+           Console.WriteLine("You Win!");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
